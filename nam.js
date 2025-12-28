08de19d3aed9ded8b430b0b73f0639483a5567b7
Md. Sadi Hasan <114307116+sadi-hasan@users.noreply.github.com>
2025-12-28 11:24:49 +0600

    Create nam.js

---
+++ nam.js
@@ -0,0 +1,58 @@
+console.log("SADI")
+let a='"SADI" is ist',b="doll is made of plastic",c="wolf",d=123.0055
+console.log(a.length)
+console.log(b.length)
+console.log(c.length)
+console.log(typeof(b))
+console.log(typeof(d))
+//indexOf
+console.log(a.indexOf("S"))
+console.log(a.indexOf(""))
+//charAt
+console.log(a.charAt(3))
+console.log(c.charAt(2))
+console.log(b.charAt(8))  //space soho count kore
+console.log(b.charAt(11))
+//slice
+console.log(b.slice(1,2))
+console.log(b.slice(0,9))  //of nai;becareful just SLIC
+//trim
+let b1='apple           '
+console.log(b1.trim())
+console.log(b.trim())
+//charCodeAt
+
+let xMen="power",iceMen='2114',u='12.032'
+console.log(typeof(xMen))
+console.log(xMen.charCodeAt(5))  //unicode shown
+console.log(xMen.charCodeAt(3))
+console.log(iceMen.charCodeAt(2))
+console.log(xMen.slice(0,6))
+console.log(u.indexOf('.'))
+console.log(xMen.indexOf("r"))
+console.log(xMen.slice(0,6))
+
+///for loop
+
+console.log("hello world")
+console.log("${a} studnt")
+
+
+for(let a=20; a>1; a=a-1){
+    console.log(a)
+}
+
+
+for(let a=30; a>1; a=a-2){
+    
+    console.log("my number is a ${a}")
+    
+}
+
+
+
+
+
+
+
+
